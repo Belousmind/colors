0bde0d8220566ce864155948c12e185b86b38bff
belousmind <belousmind@gmail.com>
2025-07-15 18:28:44 +0300

    feat: add slider

---
+++ src/components/slider/slider.js
@@ -0,0 +1,68 @@
+const slidesData = [
+  "/slide.png",
+  "/slide.png",
+  "/slide.png",
+  "/slide.png",
+  "/slide.png",
+  "/slide.png",
+];
+
+const sliderContainer = document.getElementById("sliderContainer");
+const dotsContainer = document.getElementById("dotsContainer");
+
+const slideTemplate = document.getElementById("slide-template");
+const dotTemplate = document.getElementById("dot-template");
+
+slidesData.forEach((src, index) => {
+  const slideClone = slideTemplate.content.cloneNode(true);
+  const slideImg = slideClone.querySelector("img");
+  slideImg.src = src;
+  sliderContainer.appendChild(slideClone);
+
+  const dotClone = dotTemplate.content.cloneNode(true);
+  const dot = dotClone.querySelector("li");
+  dot.classList.toggle("active", index === 0);
+  dot.dataset.index = index;
+  dotsContainer.appendChild(dot);
+});
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".category-hero__slide-image");
+  const dots = document.querySelectorAll(".category-hero__dot");
+
+  const slideWidth = slides[0].clientWidth;
+  sliderContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+
+  currentIndex = index;
+}
+
+document
+  .querySelector(".category-hero__nav--prev")
+  .addEventListener("click", () => {
+    const totalSlides = slidesData.length;
+    const newIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(newIndex);
+  });
+
+document
+  .querySelector(".category-hero__nav--next")
+  .addEventListener("click", () => {
+    const totalSlides = slidesData.length;
+    const newIndex = (currentIndex + 1) % totalSlides;
+    showSlide(newIndex);
+  });
+
+dotsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("category-hero__dot")) {
+    const index = parseInt(e.target.dataset.index, 10);
+    showSlide(index);
+  }
+});
+
+window.addEventListener("resize", () => showSlide(currentIndex));
--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import "./style.scss";
 import "./components/header/header";
 import "./components/filters/filters";
 import "./components/sorting/sorting";
+import "./components/slider/slider";
 
 import colors from "../colors.json";
 
 import { CardsRender } from "./components/product-card/product-card";
 
-
 CardsRender(colors);
 
 // console.log(window.innerWidth)
-
