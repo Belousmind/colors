ffa973f5887f1d548ddaaabbe23582c7784b5811
belousmind <belousmind@gmail.com>
2025-07-16 18:53:42 +0300

    refactor: deduplicate styles and rename slider classes

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -30,8 +30,8 @@ slidesData.forEach((src, index) => {
 let currentIndex = 0;
 
 function showSlide(index) {
-  const slides = document.querySelectorAll(".category-hero__slide-image");
-  const dots = document.querySelectorAll(".category-hero__dot");
+  const slides = document.querySelectorAll(".slider__slide-image");
+  const dots = document.querySelectorAll(".slider__dot");
 
   const slideWidth = slides[0].clientWidth;
   sliderContainer.style.transform = `translateX(-${index * slideWidth}px)`;
@@ -43,24 +43,20 @@ function showSlide(index) {
   currentIndex = index;
 }
 
-document
-  .querySelector(".category-hero__nav--prev")
-  .addEventListener("click", () => {
-    const totalSlides = slidesData.length;
-    const newIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    showSlide(newIndex);
-  });
+document.querySelector(".slider__nav--prev").addEventListener("click", () => {
+  const totalSlides = slidesData.length;
+  const newIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(newIndex);
+});
 
-document
-  .querySelector(".category-hero__nav--next")
-  .addEventListener("click", () => {
-    const totalSlides = slidesData.length;
-    const newIndex = (currentIndex + 1) % totalSlides;
-    showSlide(newIndex);
-  });
+document.querySelector(".slider__nav--next").addEventListener("click", () => {
+  const totalSlides = slidesData.length;
+  const newIndex = (currentIndex + 1) % totalSlides;
+  showSlide(newIndex);
+});
 
 dotsContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("category-hero__dot")) {
+  if (e.target.classList.contains("slider__dot ")) {
     const index = parseInt(e.target.dataset.index, 10);
     showSlide(index);
   }
